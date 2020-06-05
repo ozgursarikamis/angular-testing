d5a15050d84928dcd2b7f3109af7497ccc8bdced
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-06-06 02:29:38 +0300

    testing component methods with parameter: toHaveBeenCalledWith

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -27,13 +27,13 @@ describe('HeroesComponent', () => {
       expect(component.heroes.length).toBe(2);
     });
 
-    xit('should call deleteHero', () => {
+    it('should call deleteHero', () => {
       mockHeroService.deleteHero.and.returnValue(of(true));
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
 
-      expect(mockHeroService.deleteHero).toHaveBeenCalled();
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
     });
   });
 })
