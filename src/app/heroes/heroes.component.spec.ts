e9d8152cd86370d6c6d7a8c49ae4ef17ed8a37e8
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-06-06 02:19:33 +0300

    testing component

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,30 @@
+import { HeroesComponent } from "./heroes.component";
+import { of } from "rxjs";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'SpiderDude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'SpiderDude', strength: 55 },
+    ];
+
+    // creating mock hero service so we could use these methods:
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the list of heroes', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    });
+  });
+})
