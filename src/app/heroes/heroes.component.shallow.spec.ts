44cf965a4f7ed930998412df38a65ac254b905ea
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-06-08 01:03:54 +0300

    mocking services

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,15 +1,39 @@
 import { HeroesComponent } from './heroes.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { of } from 'rxjs/internal/observable/of';
+import { Hero } from '../hero';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService: any;
+  let heroes: Hero[];
 
   beforeEach(() => {
+    heroes = [
+      { id: 1, name: 'SpiderDude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'SuperDude', strength: 55 }
+    ];
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent]
+      declarations: [HeroesComponent],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     });
 
     // -> ngOnInit gets called automatically after this:
     fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(heroes));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
   });
 });
