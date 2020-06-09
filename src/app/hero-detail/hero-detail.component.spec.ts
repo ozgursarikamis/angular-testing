c5f60f050fa62c438b39f42e3ca30e0d0771d5db
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-06-10 01:49:59 +0300

    it fails because it doesn't know what ngModel is

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,11 +1,14 @@
 import { HeroDetailComponent } from './hero-detail.component';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { HeroService } from '../hero.service';
 import { Location } from '@angular/common';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('HeroDetailCompoent', () => {
-  let fixture, mockActivatedRoute, mockHeroService, mockLocation;
+  let fixture: ComponentFixture<HeroDetailComponent>;
+  let mockActivatedRoute, mockHeroService, mockLocation;
+
   beforeEach(() => {
     mockActivatedRoute = {
       snapshot: {
@@ -25,5 +28,11 @@ describe('HeroDetailCompoent', () => {
     });
 
     fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'SuperDude', strength: 100}));
+  });
+
+  it('should render the hero name in a h2 tag', () => {
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SuperDude'.toLocaleUpperCase());
   });
 });
