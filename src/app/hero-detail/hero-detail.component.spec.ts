bf9bfa836dd3a9eac1a7201281622032d24a3af8
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-06-11 01:25:01 +0300

    testing async code: flush method

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { HeroDetailComponent } from './hero-detail.component';
-import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, tick, flush } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { HeroService } from '../hero.service';
 import { Location } from '@angular/common';
@@ -47,7 +47,10 @@ describe('HeroDetailCompoent', () => {
       // // fails because test is sync and code is async
       // expect(mockHeroService.updateHero).toHaveBeenCalled();
 
-      tick(250);
+      // tick(250);
+
+      // if we don't know how long we have to wait:
+      flush(); // finish the waiting task(s)
       expect(mockHeroService.updateHero).toHaveBeenCalled();
     })
   );
